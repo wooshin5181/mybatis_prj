a37960a5ec97503f2f21e1ee177e2913f6b16140
wooshin5181 <rurouni5181@hanmail.net>
2025-05-30 11:39:26 +0900

    first commit

---
+++ src/main/java/day0530/HelloMyBatis.java
@@ -0,0 +1,49 @@
+package day0530;
+
+import java.io.File;
+import java.io.IOException;
+import java.io.Reader;
+import java.util.List;
+
+import org.apache.ibatis.io.Resources;
+import org.apache.ibatis.session.SqlSession;
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.apache.ibatis.session.SqlSessionFactoryBuilder;
+
+public class HelloMyBatis {
+	
+	public void useMyBatis() {
+		
+		try {
+			//1. 설정용 XML과 연결
+			Reader reader=Resources.getResourceAsReader("day0530/mybatis-config.xml");
+			//2. MyBatis Framework 생성
+//			SqlSessionFactoryBuilder ssfb=new SqlSessionFactoryBuilder();
+//			SqlSessionFactory ssf=ssfb.build(reader);
+			
+			SqlSessionFactory ssf=
+					new SqlSessionFactoryBuilder().build(reader);
+			
+			//3.MyBatis Handler 얻기
+			SqlSession ss=ssf.openSession();
+			//4. Handler를 사용한 조작
+			List<Dept> list=ss.selectList("kr.co.sist.hello.selectAllDept");
+			for( Dept dept : list) {
+				System.out.println(  dept.getDeptno()+" / " + dept.getDname()
+				+" / "+ dept.getLoc());
+			}//end for
+			
+			//5.MyBatis Handler 닫기
+			ss.close();
+			
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		
+	}//useMyBatis
+
+	public static void main(String[] args) {
+		new HelloMyBatis().useMyBatis();
+	}//main
+
+}//class
