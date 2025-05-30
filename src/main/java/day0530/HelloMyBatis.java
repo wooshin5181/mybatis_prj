a5c404a2fbcfaaa222281347dd6dd2b9a675f4aa
wooshin5181 <rurouni5181@hanmail.net>
2025-05-30 14:40:33 +0900

    테스트

--- src/main/java/day0530/HelloMyBatis.java
+++ src/main/java/day0530/HelloMyBatis.java
@@ -27,7 +27,11 @@ public class HelloMyBatis {
 			//3.MyBatis Handler 얻기
 			SqlSession ss=ssf.openSession();
 			//4. Handler를 사용한 조작
-			List<Dept> list=ss.selectList("kr.co.sist.hello.selectAllDept");
+			//MyBatis Handler(ss)가 namespace와 id명을 결합하여 node를 찾고 parsing 
+//			List<Dept> list=ss.selectList("kr.co.sist.hello.selectAllDept");
+			
+			//MyBatis Handler(ss)가 namespace는 생략가능 
+			List<Dept> list=ss.selectList("selectAllDept");
 			for( Dept dept : list) {
 				System.out.println(  dept.getDeptno()+" / " + dept.getDname()
 				+" / "+ dept.getLoc());
